4b38ebd2c6dd2b02c6f024fc5a829686cc9d3981
Onlyadmirer <akmalrbc6@gmail.com>
2026-04-30 10:19:39 +0800

    feat: add cart & cart item struct

---
+++ internal/entity/cart.go
@@ -0,0 +1,19 @@
+package entity
+
+import "time"
+
+type Cart struct {
+	ID        int       `json:"id"`
+	UserID    int       `json:"user_id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+type CartItem struct {
+	ID        int       `json:"id"`
+	CartID    int       `json:"cart_id"`
+	ProductID int       `json:"product_id"`
+	Quantity  int       `json:"quantity"`
+	Product   *Product  `json:"product,omitempty"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
