1c99557979ddb2dba6d7a40a3c726a55c5e10508
Onlyadmirer <akmalrbc6@gmail.com>
2026-05-07 09:21:47 +0800

    feat: add pagination struct

---
+++ pkg/entity/pagination.go
@@ -0,0 +1,13 @@
+package entity
+
+type PaginateMeta struct {
+	CurrentPage int `json:"current_page"`
+	TotalPages  int `json:"total_pages"`
+	TotalItems  int `json:"total_items"`
+	Limit       int `json:"limit"`
+}
+
+type PaginatedProductResponse struct {
+	Data     []Product    `json:"data"`
+	Metadata PaginateMeta `json:"metadata"`
+}
