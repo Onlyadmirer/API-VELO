e1243595b24721d68ad63fc1f25d4a0b8f54b87c
Onlyadmirer <akmalrbc6@gmail.com>
2026-05-08 16:58:48 +0800

    feat: (rbac) update unit test rbac

--- pkg/middleware/rbac_middleware_test.go
+++ pkg/middleware/rbac_middleware_test.go
@@ -30,4 +30,19 @@ func TestRBACMiddleware(t *testing.T) {
 		assert.Equal(t, http.StatusOK, rec.Code, "gagal: Admin harusnya dapat 200 ok")
 	})
 
+	// customer
+	t.Run("Customer dilarang masuk", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodDelete, "/api/products/1", nil)
+
+		ctx := context.WithValue(req.Context(), RoleKey, "customer")
+		req = req.WithContext(ctx)
+
+		rec := httptest.NewRecorder()
+
+		middlewareToTest.ServeHTTP(rec, req)
+
+		assert.Equal(t, http.StatusForbidden, rec.Code, "gagal: customer seharusnya 401")
+
+	})
+
 }
