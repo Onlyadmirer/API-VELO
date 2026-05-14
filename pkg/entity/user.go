d276ebe505bfe30c5f51015be73d075c831b6c95
Onlyadmirer <akmalrbc6@gmail.com>
2026-05-14 22:02:42 +0800

    feat: (user) update struct user.go

--- pkg/entity/user.go
+++ pkg/entity/user.go
@@ -4,19 +4,22 @@ import "time"
 
 // User merupakan model basis data utama untuk data pengguna.
 type User struct {
-	ID        int       `json:"id"`
-	Name      string    `json:"name"`
-	Email     string    `json:"email"`
-	Password  string    `json:"-"`
-	Role      string    `json:"role"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID         int    `json:"id"`
+	Name       string `json:"name"`
+	Email      string `json:"email"`
+	Password   string `json:"-"`
+	Role       string `json:"role"`
+	IsVerified bool
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
 }
 
 type RegisterUser struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	Token     string
+	ExpiresAt time.Time
 }
 type LoginUser struct {
 	Email    string `json:"email"`
