aa3d197cc5e538339641da7a1433deac5bb239cf
Onlyadmirer <akmalrbc6@gmail.com>
2026-05-15 18:44:31 +0800

    feat: Refactoring cart_repo

--- pkg/repository/cart_repository.go
+++ pkg/repository/cart_repository.go
@@ -4,7 +4,6 @@ import (
 	"VELO-backend/pkg/entity"
 	"database/sql"
 	"fmt"
-	"log"
 )
 
 // CartRepository mengelola operasi entri data keranjang belanja.
@@ -27,21 +26,20 @@ func NewCartRepository(db *sql.DB) CartRepository {
 
 // GetOrCreateCart akan membuat entri keranjang baru jika user belum memiliki keranjang yang aktif.
 func (r *cartRepository) GetOrCreateCart(userId int) (int, error) {
-	query := `SELECT id FROM carts WHERE user_id = $1`
+	query := `
+		INSERT INTO carts (user_id)
+		VALUES ($1)
+
+		ON CONFLICT (user_id)
+		DO UPDATE SET user_id = EXCLUDED.user_id
+
+		RETURNING id
+	`
 	var id int
 	err := r.db.QueryRow(query, userId).Scan(&id)
-	if err == sql.ErrNoRows {
-		queryInsert := `INSERT INTO carts (user_id) VALUES ($1) RETURNING id`
-		err := r.db.QueryRow(queryInsert, userId).Scan(&id)
-		if err != nil {
-			return 0, fmt.Errorf("gagal ambil id: %v", err)
-		}
-
-		return id, nil
-	}
 
 	if err != nil {
-		return 0, fmt.Errorf("gagal mencari cart: %v", err)
+		return 0, fmt.Errorf("gagal get/create cart: %w", err)
 	}
 
 	return id, nil
@@ -49,10 +47,20 @@ func (r *cartRepository) GetOrCreateCart(userId int) (int, error) {
 
 // UpsertCartItem memperbarui jumlah (quantity) item jika sudah ada, atau menambahkannya bila belum ada.
 func (r *cartRepository) UpsertCartItem(cartID int, productID int, quantity int) error {
-	query := `INSERT INTO cart_items (cart_id, product_id, quantity) VALUES ($1, $2, $3) ON CONFLICT (cart_id, product_id) DO UPDATE SET quantity = cart_items.quantity + EXCLUDED.quantity`
+	query := `
+		INSERT INTO cart_items (
+			cart_id,
+			product_id,
+			quantity
+		)
+		VALUES ($1, $2, $3)
+		ON CONFLICT (cart_id, product_id)
+		DO UPDATE SET
+			quantity = cart_items.quantity + EXCLUDED.quantity
+	`
 	_, err := r.db.Exec(query, cartID, productID, quantity)
 	if err != nil {
-		return fmt.Errorf("gagal tambah item ke cart: %v", err)
+		return fmt.Errorf("gagal tambah item ke cart: %w", err)
 	}
 
 	return nil
@@ -62,18 +70,27 @@ func (r *cartRepository) UpsertCartItem(cartID int, productID int, quantity int)
 // GetCart mengambil semua baris data barang di dalam keranjang belanja milik user.
 func (r *cartRepository) GetCart(userId int) ([]entity.CartItemResponse, error) {
 
-	var cartItem []entity.CartItemResponse
-	var totalPrice float64
+	var cartItems []entity.CartItemResponse
 
-	query := `SELECT ci.id, ci.cart_id, ci.quantity, p.id, p.name, p.price
-	FROM cart_items ci
-	JOIN carts c ON ci.cart_id = c.id
-	JOIN products p ON ci.product_id = p.id
-	WHERE c.user_id = $1`
+	query := `
+		SELECT
+			ci.id,
+			ci.cart_id,
+			ci.quantity,
+			p.id,
+			p.name,
+			p.price
+		FROM cart_items ci
+		JOIN carts c
+			ON ci.cart_id = c.id
+		JOIN products p
+			ON ci.product_id = p.id
+		WHERE c.user_id = $1
+	`
 
 	rows, err := r.db.Query(query, userId)
 	if err != nil {
-		return nil, fmt.Errorf("gagal query ke database: %v", err)
+		return nil, fmt.Errorf("gagal query ke database: %w", err)
 	}
 
 	defer rows.Close()
@@ -83,12 +100,22 @@ func (r *cartRepository) GetCart(userId int) ([]entity.CartItemResponse, error) 
 
 		ci.Product = &entity.Product{}
 
-		if err := rows.Scan(&ci.ID, &ci.CartID, &ci.Quantity, &ci.Product.ID, &ci.Product.Name, &ci.Product.Price); err != nil {
-			log.Println("error saat scan baris cart items: ", err)
-			continue
+		err := rows.Scan(
+			&ci.ID,
+			&ci.CartID,
+			&ci.Quantity,
+			&ci.Product.ID,
+			&ci.Product.Name,
+			&ci.Product.Price)
+
+		if err != nil {
+			return nil, fmt.Errorf(
+				"gagal scan cart item: %w",
+				err,
+			)
 		}
 
-		totalPrice = float64(ci.Quantity) * float64(ci.Product.Price)
+		totalPrice := float64(ci.Quantity) * float64(ci.Product.Price)
 
 		resp := entity.CartItemResponse{
 			ID:       ci.ID,
@@ -102,12 +129,12 @@ func (r *cartRepository) GetCart(userId int) ([]entity.CartItemResponse, error) 
 			TotalAmount: totalPrice,
 		}
 
-		cartItem = append(cartItem, resp)
+		cartItems = append(cartItems, resp)
 	}
 
 	if err = rows.Err(); err != nil {
-		return nil, fmt.Errorf("terjadi kesalahan saat membaca baris data: %v", err)
+		return nil, fmt.Errorf("terjadi kesalahan saat membaca baris data: %w", err)
 	}
 
-	return cartItem, nil
+	return cartItems, nil
 }
